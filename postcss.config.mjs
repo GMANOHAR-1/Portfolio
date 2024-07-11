2b34b5a7bd76ce4fe41f26e0a4b1a61994cb70c6
GMANOHAR-1 <122255843+GMANOHAR-1@users.noreply.github.com>
2024-07-12 00:37:09 +0530

    updated all parts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,11 @@
-module.exports = {
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer,
   },
 };
+
+export default config;
